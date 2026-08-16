2ef4e414072a4e7b4a5dff6ac6a7601cffe34df7
DeniseLH1 <jdlhprincesa@gmail.com>
2026-08-16 16:32:49 -0600

    feat(repositories): se implemento repositorio a proyecto

---
+++ src/repositories/projectRepository.js
@@ -0,0 +1,26 @@
+import { BaseRepository } from './baseRepository.js';
+
+export class ProjectRepository extends BaseRepository {
+    constructor(db) {
+        super(db, 'projects');
+    }
+
+    async findByClientId(clientId, options = {}) {
+        const objectId = this._toObjectId(clientId);
+        return await this.collection.find({ clientId: objectId }, options).toArray();
+    }
+
+    async addAdvance(projectId, advanceData, options = {}) {
+        const objectId = this._toObjectId(projectId);
+        return await this.collection.findOneAndUpdate(
+        { _id: objectId },
+        { 
+            $push: { avances: { ...advanceData, fecha: new Date() } },
+            $set: { updatedAt: new Date() }
+        },
+        { returnDocument: 'after', ...options }
+        );
+    }
+}
+
+export default ProjectRepository;
