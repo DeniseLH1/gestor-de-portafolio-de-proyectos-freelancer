32627c5be1bf1083a15c129ecaef112132b68f71
DeniseLH1 <jdlhprincesa@gmail.com>
2026-08-17 08:32:03 -0600

    git commit -m "feat(db): agregar funcion helper toObjectId para sanitizar y validar IDs"

--- config/db.js
+++ config/db.js
@@ -1,32 +1,22 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 try {
   process.loadEnvFile();
-} catch {
-  
-}
+} catch {}
 
 const { MONGODB_URI, DB_NAME } = process.env;
 
-if (!MONGODB_URI) {
-  throw new Error("Falta la variable de entorno MONGODB_URI");
-}
-
-if (!DB_NAME) {
-  throw new Error("Falta la variable de entorno DB_NAME");
-}
+if (!MONGODB_URI) throw new Error("Falta la variable de entorno MONGODB_URI");
+if (!DB_NAME) throw new Error("Falta la variable de entorno DB_NAME");
 
 const client = new MongoClient(MONGODB_URI);
-
 let db;
 
 export async function connectDB() {
   if (db) return db;
-
   await client.connect();
   db = client.db(DB_NAME);
   console.log(`Conectado a MongoDB Atlas: base de datos "${DB_NAME}"`);
-
   return db;
 }
 
@@ -39,4 +29,16 @@ export async function closeDB() {
   db = undefined;
 }
 
+
+// Para convertir un string a ObjectId de MongoDB limpiando espacios y devolver un null si la entrada no es válida
+export function toObjectId(id) {
+  if (!id || typeof id !== "string") return null;
+  const cleanId = id.trim();
+  if (cleanId.length !== 24 || !ObjectId.isValid(cleanId)) {
+    return null;
+  }
+  return new ObjectId(cleanId);
+}
+
+export { ObjectId };
 export default connectDB;
