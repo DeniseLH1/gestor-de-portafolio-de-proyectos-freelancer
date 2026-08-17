514df45c91ba241ed0c8b2a575a01c31d4fd5c1b
juanlema-16 <juanlema160703@gmail.com>
2026-08-17 10:32:42 -0600

    Revert "fix(submenus): usar cli-table3 en vez de console.table para que los colores de chalk se vean bien en deliverableSubmenu"
    This reverts commit 11bbb84dc24d4f968bd43830da59b44116bf796e.

--- src/submenus/deliverableSubmenu.js
+++ src/submenus/deliverableSubmenu.js
@@ -1,5 +1,4 @@
 import inquirer from 'inquirer';
-import Table from 'cli-table3';
 import { formatEstado, formatFecha, exito, error } from '../utils/format.js';
 
 export async function deliverableMenu(commandFactory) {
@@ -40,16 +39,14 @@ export async function deliverableMenu(commandFactory) {
           const comando = commandFactory.create('listar-entregables');
           const entregables = await comando.execute();
           console.log('\n--- Lista de Entregables ---');
-          const table = new Table({ head: ['ID', 'Descripción', 'Fecha Límite', 'Estado'] });
-          entregables.forEach((e) => {
-            table.push([
-              e._id.toString(),
-              e.descripcion,
-              formatFecha(e.fechaLimite),
-              formatEstado(e.status),
-            ]);
-          });
-          console.log(table.toString());
+          console.table(
+            entregables.map((e) => ({
+              id: e._id.toString(),
+              descripcion: e.descripcion,
+              fechaLimite: formatFecha(e.fechaLimite),
+              status: formatEstado(e.status),
+            })),
+          );
           break;
         }
 
