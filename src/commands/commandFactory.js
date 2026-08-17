c6dc92c47e4feb5e04d7ba0183c9dc1032d53965
juanlema-16 <juanlema160703@gmail.com>
2026-08-17 02:20:38 -0600

    feat(commands): registrar los 9 comandos en CommandFactory

--- src/commands/commandFactory.js
+++ src/commands/commandFactory.js
@@ -1,4 +1,12 @@
 import CreateClientCommand from './createClientCommand.js';
+import ListClientsCommand from './listClientsCommand.js';
+import FindClientCommand from './findClientCommand.js';
+import CreateContractCommand from './createContractCommand.js';
+import ListContractsCommand from './listContractsCommand.js';
+import FindContractCommand from './findContractCommand.js';
+import CreateDeliverableCommand from './createDeliverableCommand.js';
+import ListDeliverablesCommand from './listDeliverablesCommand.js';
+import FindDeliverableCommand from './findDeliverableCommand.js';
 
 class CommandFactory {
   constructor(services) {
@@ -9,6 +17,22 @@ class CommandFactory {
     switch (commandName) {
       case 'crear-cliente':
         return new CreateClientCommand(this.services.clientService);
+      case 'listar-clientes':
+        return new ListClientsCommand(this.services.clientService);
+      case 'buscar-cliente':
+        return new FindClientCommand(this.services.clientService);
+      case 'crear-contrato':
+        return new CreateContractCommand(this.services.contractService);
+      case 'listar-contratos':
+        return new ListContractsCommand(this.services.contractService);
+      case 'buscar-contrato':
+        return new FindContractCommand(this.services.contractService);
+      case 'crear-entregable':
+        return new CreateDeliverableCommand(this.services.deliverableService);
+      case 'listar-entregables':
+        return new ListDeliverablesCommand(this.services.deliverableService);
+      case 'buscar-entregable':
+        return new FindDeliverableCommand(this.services.deliverableService);
       default:
         throw new Error(`No existe ningún comando llamado "${commandName}".`);
     }
