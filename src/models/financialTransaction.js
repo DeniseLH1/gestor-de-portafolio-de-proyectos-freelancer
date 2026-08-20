531098b26433b0aab494e8026b6b63041a24a357
juanlema-16 <juanlema160703@gmail.com>
2026-08-20 12:03:53 -0600

    fix(models): exportar NUMERIC_ID_REGEX en financialTransaction.js

--- src/models/financialTransaction.js
+++ src/models/financialTransaction.js
@@ -1,6 +1,6 @@
 import BaseModel from './baseModels.js';
 
-const NUMERIC_ID_REGEX = /^[0-9]+$/;
+export const NUMERIC_ID_REGEX = /^[0-9]+$/;
 const TIPOS_TRANSACCION = ['INCOME', 'EXPENSE'];
 
 class Transaction extends BaseModel {
