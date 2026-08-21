533fbb77463832e20b95ef42529ee21c7c2a939d
juanlema-16 <juanlema160703@gmail.com>
2026-08-21 10:40:02 -0600

    feat(repositories): agregar findByName a ClientRepository

--- src/repositories/clientRepository.js
+++ src/repositories/clientRepository.js
@@ -9,18 +9,19 @@ export class ClientRepository extends BaseRepository {
         return await this.collection.findOne({ email }, options);
     }
 
-  // Búsqueda por campo 'id' personalizado
+    async findByName(nombre, options = {}) {
+        return await this.collection.find({ nombre: { $regex: nombre, $options: 'i' } }, options).toArray();
+    }
+
     async findOne(query, options = {}) {
         return await this.collection.findOne(query, options);
     }
 
-  // Actualización usando el 'id' 
     async updateByCustomId(id, data) {
         const res = await this.collection.updateOne({ id: Number(id) }, { $set: data });
         return res.modifiedCount > 0;
     }
 
-  // Eliminación usando el 'id' 
     async deleteByCustomId(id) {
         const res = await this.collection.deleteOne({ id: Number(id) });
         return res.deletedCount > 0;
